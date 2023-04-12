09ddd65be0dbbfb5318d59f84edd6ac0897861b6
Airidas <60854456+Airidas-Fix@users.noreply.github.com>
2023-04-12 22:12:56 +0100

    export rename

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,5 @@
 -- Variables
-local QBCore = exports['qb-core']:GetCoreObject()
+local QBCore = exports['qbx-core']:GetCoreObject()
 local PlayerData = QBCore.Functions.GetPlayerData()
 local CurrentWeaponData, CanShoot, MultiplierAmount = {}, true, 0
 
